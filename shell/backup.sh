8b646b9e78ea7f44511953cce27765dd40dc881b
1fabiopereira <fabio.pereira.gti@hotmail.com>
2016-11-04 13:29:04 -0200

    add mysql backup

--- shell/backup.sh
+++ shell/backup.sh
@@ -1,4 +1,29 @@
 #!/bin/sh
+                                  #
+
+# Definindo parametros do MySQL
+echo "  -- Definindo parametros do MySQL ..."
+DB_NAME='dbname'
+DB_USER='dbuser'
+DB_PASS='dbpass'
+DB_PARAM='--add-drop-table --add-locks --extended-insert --single-transaction -quick'
+
+# Definindo parametros do sistema
+echo "  -- Definindo parametros do sistema ..."
+DATE=`date +%Y-%m-%d`
+MYSQLDUMP=/usr/bin/mysqldump
+BACKUP_DIR=/backup/mysql
+BACKUP_NAME=mysql-$DATE.sql
+BACKUP_TAR=mysql-$DATE.tar
+
+#Gerando arquivo sql
+echo "  -- Gerando Backup da base de dados $DB_NAME em $BACKUP_DIR/$BACKUP_NAME ..."
+$MYSQLDUMP $DB_NAME $DB_PARAM -u $DB_USER -p$DB_PASS > $BACKUP_DIR/$BACKUP_NAME
+
+# Compactando arquivo em tar
+echo "  -- Compactando arquivo em tar ..."
+tar -cf $BACKUP_DIR/$BACKUP_TAR -C $BACKUP_DIR $BACKUP_NAME
+
 
 
 #Configuracao para data no arquivo de backup
